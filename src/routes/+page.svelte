e164d620a22d1f9ac404f4d5a85d8c9d1a8fa2f4
clau555 <netlucas@free.fr>
2024-11-29 23:15:02 +0100

    feat: new navbar
    responsive navbar, plus various UI style and formatting improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,11 @@
 <script lang="ts">
     import Title from "../components/Title.svelte";
     import AutoTyper from "../components/AutoTyper.svelte";
-    import ButtonsBar from "../components/ButtonsBar.svelte";
-    import Button from "../components/Button.svelte";
-    import ThemeSwitcher from "../components/ThemeSwitcher.svelte";
 </script>
 
 <section>
     <Title />
-    <ButtonsBar>
-        <Button label="github" link="https://github.com/clau555" />
-        <Button label="artstation" link="" />
-        <Button label="linkedin" link="https://www.linkedin.com/in/lucas-chardonnet/" />
-        <ThemeSwitcher />
-    </ButtonsBar>
     <AutoTyper />
-    <div>
-        <Button label="contact me" link="mailto:lucas.chardonnet1@gmail.com" isHighlighted />
-    </div>
 </section>
 
 <style>
@@ -26,12 +14,8 @@
         display: flex;
         flex-direction: column;
         gap: 2em;
-        max-width: 32em;
         min-height: 100%;
-    }
-
-    div {
-        display: flex;
+        max-width: 600px;
     }
 
     @media (orientation: portrait) {
